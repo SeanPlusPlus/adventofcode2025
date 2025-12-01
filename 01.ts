3f45476721ca86ee4ddc0729fdbbfb9c32f8a5b3
Sean Stephenson <seanmichaelstephenson@gmail.com>
2025-12-01 12:47:36 -0800

    read each line

--- 01.ts
+++ 01.ts
@@ -1,1 +1,12 @@
-console.log('Day 1')
+import * as fs from 'fs'
+
+// read each line from 01.input.txt and save each as an element in an array
+const input = fs.readFileSync('01.input.txt', 'utf-8').split('\n')
+
+console.log(input.length)
+console.log(input[0])
+console.log(input[1])
+console.log(input[2])
+console.log(input[input.length - 3])
+console.log(input[input.length - 2])
+console.log(input[input.length - 1])
