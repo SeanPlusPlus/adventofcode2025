a07daf7e84255af35bb25131ec414f1d33512946
Sean Stephenson <seanmichaelstephenson@gmail.com>
2025-12-01 14:12:30 -0800

    got it

--- 01.ts
+++ 01.ts
@@ -1,62 +1,43 @@
 import * as fs from 'fs'
 
-// enable logging
-const LOG = true
+// input data file name
+const fname = '01.input.txt'
 
-// const fname = '01.input.txt'
-const fname = '01.input.test.txt'
-
-// read each line from 01.input.txt and save each as an element in an array
+// read each line from input data and save each as an element in an array
 const input = fs.readFileSync(fname, 'utf-8').split('\n')
 
-// create an array of integers from 0 to 99
+// total positions on dial
 const DIAL = 100
-const nums: number[] = []
-for (let i = 0; i < DIAL; i++) {
-  nums.push(i)
-}
 
 // number of times dial is pointed at zero
 let zeroCount = 0
 
+// starting position
 let pos = 50
-LOG && console.log('Starting position:', pos)
 
 for (var i = 0; i < input.length; i++) {
   const rotate = input[i].charAt(0) === 'L' ? '-' : '+'
-  const amount = parseInt(rotate + input[i].slice(1), 10)
+  const amount = parseInt(rotate + input[i].slice(1), 10) % 100
 
-  // wrap left
-  if (amount < 0 && Math.abs(amount) > pos) {
-    pos = DIAL + (pos + amount)
-    if (pos === 0) zeroCount++
-    LOG && console.log(i, pos, amount, 'left wrap')
-    continue
-  }
-
-  // move left no wrap
   if (amount < 0) {
-    pos = pos + amount
-    if (pos === 0) zeroCount++
-    LOG && console.log(i, pos, amount, 'left no wrap')
-    continue
+    // LEFT
+    if (Math.abs(amount) > pos) {
+      // left wrap
+      pos = DIAL + (pos + amount)
+    } else {
+      pos = pos + amount
+    }
+  } else {
+    // RIGHT
+    if (pos + amount >= DIAL) {
+      // right wrap
+      pos = pos + amount - DIAL
+    } else {
+      pos = pos + amount
+    }
   }
 
-  // move right wrap
-  if (amount > 0 && pos + amount >= DIAL) {
-    pos = pos + amount - DIAL
-    if (pos === 0) zeroCount++
-    LOG && console.log(i, pos, amount, 'right wrap')
-    continue
-  }
-
-  // move right no wrap
-  if (amount > 0) {
-    pos = pos + amount
-    if (pos === 0) zeroCount++
-    LOG && console.log(i, pos, amount, 'right no wrap')
-    continue
-  }
+  if (pos === 0) zeroCount++
 }
 
 console.log('Zero count:', zeroCount)
