c7701682e448612573e47fd196f2074431fee975
Sean Stephenson <seanmichaelstephenson@gmail.com>
2025-12-01 12:50:20 -0800

    test input

--- 01.ts
+++ 01.ts
@@ -1,12 +1,15 @@
 import * as fs from 'fs'
 
+// const fname = '01.input.txt'
+const fname = '01.input.test.txt'
+
 // read each line from 01.input.txt and save each as an element in an array
-const input = fs.readFileSync('01.input.txt', 'utf-8').split('\n')
+const input = fs.readFileSync(fname, 'utf-8').split('\n')
 
 console.log(input.length)
-console.log(input[0])
-console.log(input[1])
-console.log(input[2])
-console.log(input[input.length - 3])
-console.log(input[input.length - 2])
-console.log(input[input.length - 1])
+console.log('1. ' + input[0])
+console.log('2. ' + input[1])
+console.log('3. ' + input[2])
+console.log('l - 3. ' + input[input.length - 3])
+console.log('l - 2. ' + input[input.length - 2])
+console.log('l - 1. ' + input[input.length - 1])
